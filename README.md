3a081261195e2f90df2915857980f566f516a5b7
bernabaris <bernabaris1 @gmail.com>
2025-04-23 23:02:08 +0300

    init code

--- README.md
+++
@@ -1,1 +0,0 @@
-# repsy-package-manager
---
+++ repsy-app/src/test/java/com/github/bernabaris/repsyapp/RepsyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bernabaris.repsyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepsyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
